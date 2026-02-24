1ce04794a7e7f6aa5300b17edce45b3b96581e9c
Heron Sousa <heronrs1@gmail.com>
2026-02-23 23:25:23 -0300

    fix: handle route error

--- src/app.ts
+++ src/app.ts
@@ -6,14 +6,6 @@ import { RouteNotFoundError } from "./errors/not-found";
 import { BaseError } from "./errors/base-error";
 import { INTERNAL_ERROR } from "./errors/constants/messages";
 
-dotenv.config();
-
-const app = express();
-app.use(express.json());
-
-app.use("/nfce", nfceRouter);
-app.use("/auth", authRouter);
-
 const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
   if (err instanceof BaseError) {
     return res.status(err.statusCode).send({ error: err.message });
@@ -24,7 +16,15 @@ const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
   });
 };
 
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
 app.use(errorHandler);
+
+app.use("/nfce", nfceRouter);
+app.use("/auth", authRouter);
 
 app.use(async () => {
   throw new RouteNotFoundError();
